b68a18d50a2dacef115e9499667edbc5133c635f
tessxd <tessxd@gmail.com>
2022-05-09 03:47:37 +0000

    updated experiment 2

--- run_tagged_cache_sim.py
+++ run_tagged_cache_sim.py
@@ -5,6 +5,9 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt 
 import numpy as np 
 import pandas as pd 
+import seaborn as sns
+colorpalette = sns.color_palette('colorblind',n_colors=6)
+#from matplotlib.colors import ListedColormap
 
 # Cache Parameter Options (bytes)
 cache_sizes = [262144, 524288, 1048576, 2097152, 4194304, 8388608]
@@ -23,7 +26,8 @@ for cache_size in cache_sizes:
         blocks = cache_size / block_size  
         sets = blocks / cache_ways
         
-        command = "spike --ic=16:4:64 --dc=16:4:64 --l2=%s:%s:%s -p%s riscv-tests/benchmarks/mt-matmul-%s.riscv"%(sets,cache_ways,block_size,thread,num)
+        command = "spike --ic=16:4:64 --dc=16:4:64 --l2=%s:%s:%s -p%s riscv-tests/benchmarks/mt-vvadd-%s.riscv"%(sets,cache_ways,block_size,thread,num)
+        #print(command)
         result = subprocess.Popen([command], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
 
         # Save parameters and output 
@@ -48,25 +52,29 @@ print(three_df)
 print(four_df)
 print(five_df)
 
-data_df.to_csv('tagged_cache_data_cleancode.csv')
+#data_df.to_csv('tagged_cache_data_cleancode.csv')
 
+#with plt.style.context(("seaborn-colorblind",)):
 fig2 = plt.figure()
-#plt.plot(np.log(one_df['Cache Size']), one_df['Miss Rate'], label='1 threads')
-print(np)
 print(two_df['Cache Size'])
-plt.plot(np.log(one_df['Cache Size'].tolist()),one_df['Miss Rate'], color='b', label='1 enclave')
-plt.plot(np.log(two_df['Cache Size'].tolist()),two_df['Miss Rate'], color='g', label='2 enclaves')
-plt.plot(np.log(three_df['Cache Size'].tolist()),three_df['Miss Rate'], color='purple', label='3 enclaves')
-plt.plot(np.log(four_df['Cache Size'].tolist()),four_df['Miss Rate'], color='yellow', label='4 enclaves')
-plt.plot(np.log(five_df['Cache Size'].tolist()),five_df['Miss Rate'], color='red', label='5 enclaves')
-#plt.plot(sixtyfourMB_df['Block Size'],sixtyfourMB_df['Miss Rate'], label='64MB Cache')
-#plt.plot(twofiftysixMB_df['Block Size'],twofiftysixMB_df['Miss Rate'], label='256MB Cache')
-plt.title('Experiment 2')
-plt.ylabel('L2 Miss Rates (%)')
-plt.xlabel('Cache Size')
+plt.plot(np.log(one_df['Cache Size'].tolist()),one_df['Miss Rate'], color=colorpalette[0], label='1 enclave')
+plt.plot(np.log(two_df['Cache Size'].tolist()),two_df['Miss Rate'], color=colorpalette[1], label='2 enclaves')
+plt.plot(np.log(three_df['Cache Size'].tolist()),three_df['Miss Rate'], color=colorpalette[2], label='3 enclaves')
+plt.plot(np.log(four_df['Cache Size'].tolist()),four_df['Miss Rate'], color=colorpalette[3], label='4 enclaves')
+plt.plot(np.log(five_df['Cache Size'].tolist()),five_df['Miss Rate'], color=colorpalette[4], label='5 enclaves')
+
+#plt.title('4-Way LLC Multi-Threaded Vector Addition')
+#plt.title('4-Way LLC Multi-Threaded Matrix Multiplication')
+plt.title('Vector Addition with 4-Way L2 Cache', fontsize=16) 
+#plt.title('Matrix Multiplication with 4-Way L2 Cache', fontsize=16)
+
+plt.ylabel('L2 Miss Rates (%)', fontsize=16)
+plt.xlabel('Cache Size', fontsize=16)
 plt.xticks(np.log(one_df['Cache Size'].tolist()),['256KB','512KB','1MB','2MB','4MB','8MB'])
-plt.yticks(np.arange(30,80,step=10))
+plt.yticks(np.arange(10,40,step=5))
+#plt.yticks(np.arange(30,80,step=10))
 plt.legend()
-fig2.savefig('figure2.pdf')
+fig2.savefig('vvadd-tagged_experiment2.pdf')
+#fig2.savefig('mat-mult_experiment2.pdf')
 plt.close()
 
